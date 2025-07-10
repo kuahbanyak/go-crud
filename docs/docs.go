cfc498543bca4d0004739469f7d96a643af71fa6
MUHAMAD ALWI AZIZ <90122217@unitedtractors.com>
2025-07-10 20:12:12 +0700

    fix: update Swagger configuration to allow only HTTP scheme

--- docs/docs.go
+++ docs/docs.go
@@ -1192,7 +1192,7 @@ var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "localhost:8080",
 	BasePath:         "/api/v1",
-	Schemes:          []string{"http", "https"},
+	Schemes:          []string{"http"},
 	Title:            "Product Management API",
 	Description:      "This is a Product Management API server implementing Clean Architecture",
 	InfoInstanceName: "swagger",
